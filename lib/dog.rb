1ac4398d777e6a2d4c266af41ac4f21c52dae778
cedric e goddard <8ceg6@comcast.net>
2019-10-09 04:19:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+require 'pry'
+class Dog 
+  def name= dogs_name
+    @a_dogs_name=dogs_name
+  end
+  def name
+    @a_dogs_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
